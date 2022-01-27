06881ad1c9514e72fd6c8cfe37a466297977cfa7
Zebych <81613417+Zebych@users.noreply.github.com>
2022-01-27 12:47:03 +0300

    axios get posts

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/myModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'aadsf', body: 'sder'},
-        {id: 2, title: 'JdhgfgfS2', body: 'aerr'},
-        {id: 3, title: 'Sdgff3', body: 'fgdf'},
-    ])
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
 
-    //useMemo- используется когда в callback производятся какие-то вычисления
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            // метод sort мутирует объект по этому создаем копию
-            //localeCompare-сравнивает значения
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(({title}) => title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+
+    async function fetchPosts() {
+        setIsPostsLoading(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setIsPostsLoading(false)
     }
 
     const removePost = (post) => {
@@ -40,16 +39,24 @@ function App() {
 
     return (
         <div className='App'>
-            <MyButton style={{marginTop:30}} onClick={()=>setModal(true)}>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
                 Создать пользователя
             </MyButton>
+
             <MyModal visible={modal} setVisible={setModal}>
                 <PostForm create={createPost}/>
             </MyModal>
 
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList remove={removePost} posts={sortedAndSearchedPosts}
-                      title={'Список постов1'}/>
+
+            {isPostsLoading
+                ?
+                <h1>Идёт загрузка....</h1>
+                :
+                <PostList remove={removePost} posts={sortedAndSearchedPosts}
+                          title={'Список постов1'}/>
+            }
+
         </div>
     );
 }
