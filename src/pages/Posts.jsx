f27d2759f21af7f4d8350954c3ad596d193d6548
Zebych <81613417+Zebych@users.noreply.github.com>
2022-01-27 21:34:07 +0300

    number of subloads of elements per page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -12,6 +12,7 @@ import Loader from "../components/UI/loader/Loader";
 import PostList from "../components/PostList";
 import Pagination from "../components/UI/pagination/Pagination";
 import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 
 function Posts() {
@@ -19,7 +20,7 @@ function Posts() {
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const [totalPages, setTotalPages] = useState(0)
-    const [limit] = useState(10)
+    const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(1)
 
     const lastElement = useRef()
@@ -39,7 +40,7 @@ function Posts() {
 
     useEffect(() => {
         fetchPosts(limit, page)
-    }, [page])
+    }, [page, limit])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -65,6 +66,18 @@ function Posts() {
             </MyModal>
 
             <PostFilter filter={filter} setFilter={setFilter}/>
+
+            <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultValue={'количество элементов на странице'}
+                options={[
+                    {value: 5, name: '5'},
+                    {value: 10, name: '10'},
+                    {value: 25, name: '25'},
+                    {value: -1, name: 'Показать всё'},
+                ]}
+            />
 
             {postError &&
             <h1>Произошла ошибка ${postError}</h1>}
