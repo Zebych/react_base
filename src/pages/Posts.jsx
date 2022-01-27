8cbe5296286ad6e16e116fac38b8eb374ed97f5e
Zebych <81613417+Zebych@users.noreply.github.com>
2022-01-27 21:25:11 +0300

    autoload posts when a certain element appears in the visibility zone

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "../styles/App.css"
 import {usePosts} from "../hooks/usePosts";
 import {useFetching} from "../hooks/useFetching";
@@ -11,6 +11,7 @@ import PostFilter from "../components/PostFilter";
 import Loader from "../components/UI/loader/Loader";
 import PostList from "../components/PostList";
 import Pagination from "../components/UI/pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
 
 
 function Posts() {
@@ -18,28 +19,32 @@ function Posts() {
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const [totalPages, setTotalPages] = useState(0)
-    const [limit, setLimit] = useState(10)
+    const [limit] = useState(10)
     const [page, setPage] = useState(1)
 
+    const lastElement = useRef()
 
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
     const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
             const response = await PostService.getAll(limit, page)
-            setPosts(response.data)
+            setPosts([...posts, ...response.data])
             const totalCount = response.headers['x-total-count']
             setTotalPages(getPageCount(totalCount, limit))
         }
     )
 
+    useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+        setPage(page + 1)
+    })
+
     useEffect(() => {
         fetchPosts(limit, page)
-    },[])
+    }, [page])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
-
 
     const removePost = (post) => {
         setPosts(posts.filter(({id}) => id !== post.id))
@@ -47,7 +52,6 @@ function Posts() {
 
     const changePage = (page) => {
         setPage(page)
-        fetchPosts(limit, page)
     }
 
     return (
@@ -63,18 +67,18 @@ function Posts() {
             <PostFilter filter={filter} setFilter={setFilter}/>
 
             {postError &&
-            <h1>Произошла ошибка ${postError}</h1>
-            }
+            <h1>Произошла ошибка ${postError}</h1>}
 
-            {isPostsLoading
-                ?
-                <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-                    <Loader/>
-                </div>
-                :
-                <PostList remove={removePost} posts={sortedAndSearchedPosts}
-                          title={'Список постов1'}/>
-            }
+            <PostList remove={removePost} posts={sortedAndSearchedPosts}
+                      title={'Список постов1'}/>
+            <div ref={lastElement} style={{height: 20, background: 'red'}}/>
+
+            {isPostsLoading &&
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+                <Loader/>
+            </div>}
+
+
             <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
         </div>
     );
